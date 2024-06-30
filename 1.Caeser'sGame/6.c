4253e06aa578b73b8220d5714431b5887cb1c13e
VivithRK <139640508+VivithRK@users.noreply.github.com>
2024-06-30 12:23:49 +0530

    Update 6.c

--- 1.Caeser'sGame/6.c
+++ 1.Caeser'sGame/6.c
@@ -1,25 +1,33 @@
-#include<stdio.h>
-int w[10],p[10],n;
-intmax(inta,int b)
-{
-return a>b?a:b;
+#include <stdio.h>
+
+int w[10], p[10], n;
+
+int max(int a, int b) {
+    return a > b ? a : b;
 }
-intknap(inti,int m)
-{
-if(i==n) return w[i]>m?0:p[i];
-if (w[i]>m) return knap(i+1,m);
-return max(knap(i+1,m),knap(i+1,m-w[i])+p[i]);
+
+int knap(int i, int m) {
+    if (i == n) return w[i] > m ? 0 : p[i];
+    if (w[i] > m) return knap(i + 1, m);
+    return max(knap(i + 1, m), knap(i + 1, m - w[i]) + p[i]);
 }
-void main()
-{
-intm,i,max_profit;
-printf("\nEnter the number of objects: ");
-scanf("%d",&n);
-printf("\nEnter the knapsack capacity: ");
-scanf("%d",&m);
-printf("\nEnter profit followed by weight: ");
-for(i=1;i<=n;i++)
-scanf("%d%d",&p[i],&w[i]);
-max_profit=knap(1,m);
-printf("\nMax profit = %d",max_profit);
+
+int main() {
+    int m, i, max_profit;
+    
+    printf("Enter the number of objects: ");
+    scanf("%d", &n);
+    
+    printf("Enter the knapsack capacity: ");
+    scanf("%d", &m);
+    
+    printf("Enter profit followed by weight: ");
+    for (i = 1; i <= n; i++) {
+        scanf("%d %d", &p[i], &w[i]);
+    }
+    
+    max_profit = knap(1, m);
+    printf("Max profit = %d\n", max_profit);
+    
+    return 0;
 }
